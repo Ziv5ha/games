2c9a2da2f9246067ea0c4cac9f1d39c0d0d11bfd
Ziv5ha <89574596+Ziv5ha@users.noreply.github.com>
2022-08-16 13:15:38 +0300

    added tents and trees test win function to concept

--- concepts/tents-and-trees.ts
+++ concepts/tents-and-trees.ts
@@ -1,20 +1,34 @@
-const height = 6;
-const width = 6;
+const height = 10;
+const width = 5;
 
 declare namespace Game {
+  interface Game {
+    tentsNumsX: number[];
+    tentsNumsY: number[];
+    map: Map;
+  }
   type Map = string[][];
   type Row = string[];
-  type Tents = [number, number][];
+  type Tile = [number, number];
 }
 
-function generateBoard() {
+function generateBoard(height: number, width: number): Game.Game {
   const map = generateEmptyMap(height, width);
-  populateMap(map);
-  console.log(map);
-  const emptyMap = removeTents(map);
-  console.log(emptyMap);
-  return map;
+  const tentsNum = calcTentsNum(height, width);
+  populateMap(map, tentsNum);
+  const tentsNumsX = generateTentsNumsInColumnsArr(map);
+  const tentsNumsY = generateTentsNumsInRowsArr(map);
+  const treesMap = removeTents(map);
+  printGame({ map: treesMap, tentsNumsX, tentsNumsY });
+  return { map: treesMap, tentsNumsX, tentsNumsY };
 }
+
+const calcTentsNum = (width: number, height: number): number => {
+  const roundUp = Math.floor(Math.random() * 2) === 1;
+  return roundUp
+    ? Math.ceil(width * height * 0.2)
+    : Math.floor(width * height * 0.2);
+};
 
 const generateEmptyMap = (height: number, width: number): Game.Map => {
   const map = [] as Game.Map;
@@ -28,18 +42,13 @@ const generateEmptyMap = (height: number, width: number): Game.Map => {
   return map;
 };
 
-const populateMap = (map: Game.Map): Game.Map => {
-  const roundUp = Math.floor(Math.random() * 2) === 1;
-  const tentsNum = roundUp
-    ? Math.ceil(width * height * 0.2)
-    : Math.floor(width * height * 0.2);
+const populateMap = (map: Game.Map, tentsNum: number): Game.Map => {
   let placedTents = 0;
   while (placedTents < tentsNum) {
-    const y = Math.floor(Math.random() * height);
-    const x = Math.floor(Math.random() * width);
-    if (validTent(map, y, x)) {
+    const y = Math.floor(Math.random() * map.length);
+    const x = Math.floor(Math.random() * map[0].length);
+    if (validTentPlacement(map, y, x)) {
       map[y][x] = '⛺';
-      console.log('placed ⛺');
       placeTree(map, y, x);
       placedTents++;
     }
@@ -47,7 +56,7 @@ const populateMap = (map: Game.Map): Game.Map => {
   return map;
 };
 
-const validTent = (map: Game.Map, y: number, x: number): boolean => {
+const validTentPlacement = (map: Game.Map, y: number, x: number): boolean => {
   switch (y) {
     case 0:
       return (
@@ -95,7 +104,6 @@ const placeTree = (map: Game.Map, tentY: number, tentX: number) => {
   }
 
   if (map[treeY][treeX] !== '🌳' && map[treeY][treeX] !== '⛺') {
-    console.log('placed 🌳');
     map[treeY][treeX] = '🌳';
   } else {
     placeTree(map, tentY, tentX);
@@ -121,4 +129,164 @@ const removeTents = (map: Game.Map): Game.Map => {
   return map.map((row) => row.map((tile) => (tile === '⛺' ? '  ' : tile)));
 };
 
-generateBoard();
+const generateTentsNumsInRowsArr = (map: Game.Map): number[] => {
+  const tentsNumsY = [] as number[];
+  map.forEach((row) => {
+    tentsNumsY.push(row.count('⛺'));
+  });
+  return tentsNumsY;
+};
+
+const generateTentsNumsInColumnsArr = (map: Game.Map): number[] => {
+  const tentsNumsY = [] as number[];
+  for (let i = 0; i < map[0].length; i++) {
+    tentsNumsY.push(countVerticly(map, '⛺', i));
+  }
+  return tentsNumsY;
+};
+
+declare global {
+  interface Array<T> {
+    count(elem: T): number;
+  }
+}
+
+if (!Array.prototype.count) {
+  Array.prototype.count = function <T>(query: T): number {
+    let counter = 0;
+    this.forEach((elem) => {
+      if (elem === query) counter++;
+    });
+    return counter;
+  };
+}
+
+export const countVerticly = <T>(
+  grid: T[][],
+  query: T,
+  columnNum: number
+): number => {
+  let counter = 0;
+  grid.forEach((row) => {
+    if (row[columnNum] === query) counter++;
+  });
+  return counter;
+};
+
+const printGame = (game: Game.Game): void => {
+  const { tentsNumsX: tentNumsX, tentsNumsY: tentNumsY, map } = game;
+  const printReadyMap = [] as (string | number)[][];
+  printReadyMap.push([' ', ...tentNumsX.map((num) => ` ${num}`)]);
+  map.forEach((row, i) => printReadyMap.push([`${tentNumsY[i]}`, ...row]));
+  console.log(printReadyMap);
+};
+
+//////////////
+// Test win //
+//////////////
+function generateCompletedBoard(height: number, width: number): Game.Game {
+  const map = generateEmptyMap(height, width);
+  const tentsNum = calcTentsNum(height, width);
+  populateMap(map, tentsNum);
+  const tentsNumsX = generateTentsNumsInColumnsArr(map);
+  const tentsNumsY = generateTentsNumsInRowsArr(map);
+  return { map, tentsNumsX, tentsNumsY };
+}
+
+function testWin(game: Game.Game): boolean | Game.Tile {
+  printGame(game);
+  const { map } = game;
+  if (!tentsNumEqualsTreeNum(map)) return false;
+  if (!correctTentsInRowsAndColumns(game)) return false;
+  const didaYouWin = checkTentLocations(map);
+  console.log(didaYouWin ? 'you won!' : 'try again...');
+  return didaYouWin;
+}
+
+const correctTentsInRowsAndColumns = (game: Game.Game): boolean => {
+  const { tentsNumsX, tentsNumsY, map } = game;
+  const correctTentsInRows = tentsNumsY.every((correctTentNum, i) => {
+    const currentTentsInRow = map[i].count('⛺');
+    return correctTentNum === currentTentsInRow;
+  });
+  const correctTentsInColumns = tentsNumsX.every((correctTentNum, i) => {
+    const currentTentsInRow = countVerticly(map, '⛺', i);
+    return correctTentNum === currentTentsInRow;
+  });
+  return correctTentsInRows && correctTentsInColumns;
+};
+
+const tentsNumEqualsTreeNum = (map: Game.Map): boolean => {
+  let tentsNum = 0;
+  let treesNum = 0;
+  map.forEach((row) => {
+    row.forEach((tile) => {
+      if (tile === '⛺') tentsNum++;
+      if (tile === '🌳') treesNum++;
+    });
+  });
+  return treesNum === tentsNum;
+};
+
+const checkTentLocations = (map: Game.Map): true | Game.Tile => {
+  for (let y = 0; y < map.length; y++) {
+    const row = map[y];
+    for (let x = 0; x < row.length; x++) {
+      if (map[y][x] === '⛺') {
+        // check for no close tents
+        if (!TentNotNextToOtherTent(map, y, x)) return [y, x];
+        // Check every tree has a tent
+        if (!TentNextToTree(map, y, x)) return [y, x];
+      }
+    }
+  }
+  return true;
+};
+
+const TentNotNextToOtherTent = (
+  map: Game.Map,
+  y: number,
+  x: number
+): boolean => {
+  switch (y) {
+    case 0:
+      return (
+        map[y][x - 1] !== '⛺' &&
+        map[y][x + 1] !== '⛺' &&
+        map[y + 1][x - 1] !== '⛺' &&
+        map[y + 1][x] !== '⛺' &&
+        map[y + 1][x + 1] !== '⛺'
+      );
+    case height - 1:
+      return (
+        map[y - 1][x - 1] !== '⛺' &&
+        map[y - 1][x] !== '⛺' &&
+        map[y - 1][x + 1] !== '⛺' &&
+        map[y][x - 1] !== '⛺' &&
+        map[y][x + 1] !== '⛺'
+      );
+    default:
+      return (
+        map[y - 1][x - 1] !== '⛺' &&
+        map[y - 1][x] !== '⛺' &&
+        map[y - 1][x + 1] !== '⛺' &&
+        map[y][x - 1] !== '⛺' &&
+        map[y][x + 1] !== '⛺' &&
+        map[y + 1][x - 1] !== '⛺' &&
+        map[y + 1][x] !== '⛺' &&
+        map[y + 1][x + 1] !== '⛺'
+      );
+  }
+};
+
+const TentNextToTree = (map: Game.Map, y: number, x: number): boolean => {
+  return (
+    (map[y - 1] ? map[y - 1][x] !== '🌳' : false) ||
+    map[y][x - 1] !== '🌳' ||
+    map[y][x + 1] !== '🌳' ||
+    (map[y + 1] ? map[y + 1][x] !== '🌳' : false)
+  );
+};
+
+generateBoard(height, width);
+testWin(generateCompletedBoard(height, width));
